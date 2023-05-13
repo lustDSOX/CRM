ff063cc676a408ea69f4cc3fd16ed5c82331f72a
Коля Зименков <k.zimenkov@mail.ru>
2023-05-13 18:53:53 +0300

    Необходимо протестировать поиск по заявкам (в тестовом проекте вылетела ошибка, что RequesterNavigation = null, что странно)

--- CRM/Manager.cs
+++ CRM/Manager.cs
@@ -54,8 +54,15 @@ namespace CRM
         {
             using (var client = new ImapClient())
             {
-                //string connection = "imap." + receiver.MailUsername.Substring(receiver.MailUsername.IndexOf("@") + 1);
-                client.Connect("mail.cit-nnov.ru", 143, false);
+
+                try
+                {
+                    client.Connect(receiver.MailServer, 143, false);
+                }
+                catch (Exception)
+                {
+                    client.Connect(receiver.MailServer, 993, true);
+                }
                 client.Authenticate(receiver.MailUsername, receiver.UserPassword);
                 var inbox = client.GetFolder(receiver.IncommingMessageFolder);
                 inbox.Open(FolderAccess.ReadWrite);
---
+++ CRM/Search.cs
@@ -0,0 +1,49 @@
+﻿using CRM.Models;
+using System.IO.Compression;
+using System.Text;
+
+namespace CRM
+{
+    public class Search
+    {
+        public static List<Ticket> resultList = new List<Ticket>();
+        public static CrmRazorContext db = new CrmRazorContext();
+        public static List<Ticket> SearchOnTickets(string searchWord)
+        {
+            searchWord = searchWord.ToLower();
+            string ticketDescription = "";
+            foreach (var ticket in db.Tickets.ToList())
+            {
+                byte[] compressedBytes = ticket.TicketDesciption;
+                try
+                {
+                    using (var input = new MemoryStream(compressedBytes))
+                    {
+                        using (var gzip = new GZipStream(input, CompressionMode.Decompress))
+                        {
+                            using (var reader = new StreamReader(gzip))
+                            {
+                                ticketDescription = reader.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+                    string result = Encoding.UTF8.GetString(compressedBytes);
+                }
+                try
+                {
+                    if (ticket.TicketTitle.ToLower().Contains(searchWord) || ticketDescription.ToLower().Contains(searchWord) || ticket.RequesterNavigation.Email.ToLower().Contains(searchWord))
+                    {
+                        resultList.Add(ticket);
+                    }
+                }
+                catch (Exception)
+                {
+                }
+            }
+            return resultList;
+        }
+    }
+}
